d16d03e4985dce45f13a09f07cbbe6cf980d2704
howyp <howyp@users.noreply.github.com>
2015-03-26 16:57:44 +0000

    Bump version to 0.2.0-SNAPSHOT

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -22,7 +22,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings._
   import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
 
-  val appVersion = "0.1.0"
+  val appVersion = "0.2.0-SNAPSHOT"
 
   val appDependencies = Seq(
     "org.scalatest" %% "scalatest" % "2.2.4" % "test",
